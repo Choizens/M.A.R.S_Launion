8c0212150e993ee5ec41a94b6eb4a21451ec2240
Choizens <knightcyberg@gmail.com>
2026-03-05 09:05:41 +0800

    fix: switch SMTP port to 465 (SSL) to bypass Railway network outbound blocking on 587

--- mars_backend/mars_backend/settings.py
+++ mars_backend/mars_backend/settings.py
@@ -178,8 +178,8 @@ SIMPLE_JWT = {
 # (Requires 2-Step Verification enabled on your Google account)
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', 'dummychan70@gmail.com')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', 'xxeu ccbl dbug fwew')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
